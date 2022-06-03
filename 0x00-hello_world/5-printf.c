d9410c8b90ddd9db41ba866baa954ad27258577c
Eric <eric.majao@gmail.com>
2022-06-03 14:13:15 -0700

    betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,24 +1,7 @@
-#!/bin/bash
 #include <stdio.h>
-
-
-
-   /**
-
-    * main - Prints a text
-
-    *
-
-    * Return: Always 0 (Success)
-
-    */
-
 int main(void)
-
 {
-  
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  
-  return (0);
-  
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
 }
