734b7281a1a0a4c4fd1df08710a0acf01496572e
Eric <eric.majao@gmail.com>
2022-06-06 16:50:07 -0700

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,91 +1,32 @@
 #include <stdio.h>
 
-
-
 /**
-
- * main - Prints 3 combination of numbers
-
+ * main - Entry point
  *
-
- * Return: Always (Success)
-
+ * Return: Always 0 (Success)
+ *
  */
-
 int main(void)
-  
 {
-  
-  int c, i, k, j;
-  
+int i;
+int j;
 
-  
-  for (c = 48; c <= 57; c++)
-    
-    {
-      
-      for (i = 48; i <= 57; i++)
-	
-	{
-	  
-	  for (k = 48; k <= 57; k++)
-	    
-	    {
-	      
-	      for (j = 48; j <= 57; j++)
-		
-		{
-		  
-		  if (((k + j) > (c + i) &&  k >= c) || c < k)
-		    
-		    {
-		      
-		      putchar(c);
-		      
-		      putchar(i);
-		      
-		      putchar(' ');
-		      
-		      putchar(k);
-		      
-		      putchar(j);
-		      
-
-		      
-		      if (c + i + k + j == 227 && c == 57)
-			
-			{
-			  
-			  break;
-			  
-			}
-		      
-		      else
-			
-			{
-			  
-			  putchar(',');
-			  
-			  putchar(' ');
-			  
-			}
-		      
-		    }
-		  
-		}
-	      
-	    }
-	  
-	}
-      
-    }
-  
-
-  
-  putchar('\n');
-  
-
-  
-  return (0);
-  
+for (i = 0; i <= 99; i++)
+{
+for (j = i + 1; j <= 99; j++)
+{
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
+putchar (' ');
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
+if (!(i == 98 && j == 99))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
 }
